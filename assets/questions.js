83a4bd424ba5fb5ad0d4af6375a8c0a16490e50c
Danqi Yin <dy2395@columbia.edu>
2022-05-17 18:15:48 -0400

    commit2

---
+++ assets/questions.js
@@ -0,0 +1,37 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Conmmonly used data type do not include?",
+    answer: "alerts",
+    options: [
+      "strings",
+      "booleans",
+      "alerts",
+      "numbers"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The condition in an if/else statement is enclosed within____?",
+    answer: "parentheses",
+    options: [
+      "quotes",
+      "parentheses",
+      "curly brackets",
+      "square brackets"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Arrays in JavaScript can be used to store____?",
+    answer: "All of the above",
+    options: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "All of the above"
+    ]
+  },
+  
+];
--- assets/script.js
+++ assets/script.js
@@ -1,125 +1,135 @@
-// Timer
+//selecting all required elements
 
-// Quiz
+const info_box = document.querySelector(".info_box");
+const start_btn = document.querySelector(".buttons .start");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+const record_box = document.querySelector(".record_box");
+const submit_btn = document.querySelector(".submit");
+const restart_quiz = record_box.querySelector(".buttons .restart");
+const clear_highscore = record_box.querySelector(".buttons .clear");
+const formEl = document.querySelector("#record-form");
+const scoreRecordEl = document.querySelector("#score-list");
+const recordtitle = document.querySelector(".record_title");
 
-const quizContainer = document.getElementById('quiz');
+info_box.classList.add("activeInfo");
 
-const myQuestions = [
-    {
-      question: "Conmmonly used data type do not include?",
-      answers: {
-        a: "Strings",
-        b: "Booleans",
-        c: "Numbers",
-        d: "Assets"
-      },
-      correctAnswer: "d"
-    },
-    {
-      question: "Which one of these is a JavaScript package manager?",
-      answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which tool can you use to ensure code quality?",
-      answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
-      },
-      correctAnswer: "d"
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let widthValue = 0;
+let timeleft= 30;
+
+// if Start Quiz button clicked
+start_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    showQuetions(0); //calling showQestions function
+    startTimer(timeleft); //calling startTimer function
+    timeText.textContent = "Time Left"; //change the timeText to Time Left
+}
+
+const next_btn = document.querySelector("footer .next_btn");
+
+// if Next Que button clicked
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){ //if question count is less than total question length
+        que_count++; //increment the que_count value
+        que_numb++; //increment the que_numb value
+        showQuetions(que_count); //calling showQestions function
+        next_btn.classList.remove("show"); //hide the next button
+    }else{
+        clearInterval(counter); //clear counter
+        showResult(); //calling showResult function
     }
-  ];
+}
 
-  function buildQuiz(){
-    // variable to store the HTML output
-    const output = [];
-  
-    // for each question...
-    myQuestions.forEach(
-      (currentQuestion, questionNumber) => {
-  
-        // variable to store the list of possible answers
-        const answers = [];
-  
-        // and for each available answer...
-        for(letter in currentQuestion.answers){
-  
-          // ...add an HTML radio button
-          answers.push(
-            `<label>
-              <input type="radio" name="question${questionNumber}" value="${letter}">
-              ${letter} :
-              ${currentQuestion.answers[letter]}
-            </label>`
-          );
+// getting questions and options from array
+function showQuetions(index){
+    const que_text = document.querySelector(".que_text");
+
+    //creating a new span and div tag for question and option and passing the value using array index
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
+    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+    
+    const option = option_list.querySelectorAll(".option");
+
+    // set onclick attribute to all available options
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+//if user clicked on option
+function optionSelected(answer){
+    let userAns = answer.textContent; //getting user selected option
+    let correcAns = questions[que_count].answer; //getting correct answer from array
+    const allOptions = option_list.children.length; //getting all option items
+    
+    if(userAns == correcAns){ //if user selected option is equal to array's correct answer
+        userScore += 1; //upgrading score value with 1
+        answer.classList.add("selected"); //adding color to selected option
+    }
+    else{
+        answer.classList.add("selected"); //adding color to selected option
+        clearInterval(counter); 
+        startTimer(timeleft-10);//Timeleft substract 10 s wiht wrong answer
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+    }
+    next_btn.classList.add("show"); //show the next button if user selected any option
+}
+
+function showResult(){
+    quiz_box.classList.remove("activeQuiz"); //hide quiz box
+    result_box.classList.add("activeResult"); //show result box
+    const scoreText = result_box.querySelector(".score_text");
+    let scoreTag = '<span> You got <p>'+ userScore*10 +'</p> out of <p>'+ questions.length*10+'</p>.</span>';
+    scoreText.innerHTML = scoreTag;
+}
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+
+        timeCount.textContent = time; //changing the value of timeCount with time value
+        time--; //decrement the time value
+        timeleft = time;
+        if(time < 0){ //if timer is less than 0
+            clearInterval(counter); //clear counter
+            timeText.textContent = "Time Off"; //change the time text to time off
+            showResult();
         }
-  
-        // add this question and its answers to the output
-        output.push(
-          `<div class="question"> ${currentQuestion.question} </div>
-          <div class="answers"> ${answers.join('')} </div>`
-        );
-      }
-    );
-  
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join('');
-  }
-  myQuestions.forEach( (currentQuestion, questionNumber) => {
-    // the code we want to run for each question goes here
-  });
+        return timeleft;
+    }
+}
 
-  const answers = [];
+// if Submit button clicked
+submit_btn.onclick = ()=>{
+    result_box.classList.remove("activeResult"); //hide result box
+    record_box.classList.add("activeRecord"); //show record box
+    scoreFormHandler();
+}
 
-  // and for each available answer...
-  for(letter in currentQuestion.answers){
-  
-    // ...add an html radio button
-    answers.push(
-      `<label>
-        <input type="radio" name="question${questionNumber}" value="${letter}">
-        ${letter} :
-        ${currentQuestion.answers[letter]}
-      </label>`
-    );
-  }
-
-  
-  // add this question and its answers to the output
-  output.push(
-    `<div class="question"> ${currentQuestion.question} </div>
-    <div class="answers"> ${answers.join('')} </div>`
-  );
-
-  quizContainer.innerHTML = output.join('');
-
-
-
-// Final page
-
-
-
-// Highscores list page
-var scoreInput=100;
-
-var formEl = document.querySelector("#record-form");
-var scoreRecordEl = document.querySelector("#score-list");
-
-
-var scoreFormHandler = function(event) {
-  event.preventDefault();
+// Highscore record list code
+var scoreFormHandler = function() {
   var scoreNameInput = document.querySelector("input[name='score-name'").value;
 
   // check if inputs are empty (validate)
   if (scoreNameInput === "" ) {
     alert("You need to fill out your initials!");
-    return false;
+    record_box.classList.remove("activeRecord"); //remove record box
+    return showResult(); 
   }
   
   formEl.reset();
@@ -129,7 +139,7 @@ var scoreFormHandler = function(event) {
   
   var scoreDataObj = {
     name: scoreNameInput,
-    score: scoreInput,
+    score: userScore*10,
   };
 
   createScoreEl(scoreDataObj);
@@ -143,7 +153,7 @@ var createScoreEl = function(scoreDataObj) {
   // create div to hold task info and add to list item
   var scoreInfoEl = document.createElement("div");
   scoreInfoEl.className = "score-info";
-  scoreInfoEl.innerHTML = "<h3>" + scoreDataObj.name + "</h3><span>" + scoreDataObj.score + "</span>";
+  scoreInfoEl.innerHTML = "<span>"+scoreDataObj.name+"        "+scoreDataObj.score+"</span>";
   listItemEl.appendChild(scoreInfoEl);
 
   console.dir(listItemEl);
@@ -152,5 +162,11 @@ var createScoreEl = function(scoreDataObj) {
   scoreRecordEl.appendChild(listItemEl);
 };
 
-formEl.addEventListener("submit", scoreFormHandler);
-
+// if Restart Quiz button clicked
+restart_quiz.onclick = ()=>{
+    window.location.reload(); //reload the current window
+}
+// if Clear Record button clicked
+clear_highscore.onclick = ()=>{
+   scoreRecordEl.innerHTML = ""; //clear the record
+}
